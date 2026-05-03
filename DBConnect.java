1bf50418498e81aec9dfbeec6a39e2cdb2d19cf9
Your Name <you@example.com>
2026-05-03 10:30:36 +0530

    rv

--- DBConnect.java
+++ DBConnect.java
@@ -1,6 +1,6 @@
 public class DBConnect {
     public static void main(String[] args) {
-        System.out.println("DBConnect class is running...");
+        System.out.println("Hello World");
         // Here you can add code to connect to a database
     }
 }
